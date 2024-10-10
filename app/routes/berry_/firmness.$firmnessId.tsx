7611825f947b67984dcb41672277aabf4668bd7e
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-10-10 10:05:58 -0700

    update: use carousel component on firmness detail page

--- app/routes/berry_/firmness.$firmnessId.tsx
+++ app/routes/berry_/firmness.$firmnessId.tsx
@@ -1,5 +1,27 @@
+import { TypographyH2 } from "@/components/typography/headings";
+import { getBerryFirmnessQueryOptions } from "@/features/poke-berry-firmness/api/get-firmness";
+import FirmnessBerryCarousel from "@/features/poke-berry-firmness/components/firmness-berry-carousel";
+import useBerryFirmnessQuery from "@/features/poke-berry-firmness/hooks/use-berry-firmness";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/berry/firmness/$firmnessId")({
-    component: () => <div>Hello /berry/firmness/$firmnessId!</div>,
+    loader: ({ context: { queryClient }, params: { firmnessId } }) => {
+        queryClient.ensureQueryData(getBerryFirmnessQueryOptions(firmnessId));
+    },
+    component: () => <BerryFirmnessDetails />,
 });
+
+function BerryFirmnessDetails() {
+    const { firmnessId } = Route.useParams();
+    const berryQuery = useBerryFirmnessQuery({ firmnessId });
+    return (
+        <main className="flex flex-col mx-32 justify-center">
+            <TypographyH2>
+                Berries of{" "}
+                <span className="capitalize">{berryQuery.data.name}</span>{" "}
+                Firmness
+            </TypographyH2>
+            <FirmnessBerryCarousel firmness={berryQuery.data} />
+        </main>
+    );
+}
