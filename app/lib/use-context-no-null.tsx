9d5fee933b0597ff3da14b9fb59e7c003843f93e
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-10-20 12:39:07 -0700

    add: shadcn tooltip

--- app/lib/use-context-no-null.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function useContextErrorIfNull<T>(context: React.Context<T | null>): T {
-    const value = React.useContext(context);
-    if (!value) {
-        throw new Error("useContextErrorIfNull: Context value is null");
-    }
-    return value;
-}
