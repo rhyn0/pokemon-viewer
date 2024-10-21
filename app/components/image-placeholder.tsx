ac2a0e0b4788e3967da16b2e496f27c91265df0e
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-10-21 14:56:30 -0700

    update: add className prop to placeholder image

--- app/components/image-placeholder.tsx
+++ app/components/image-placeholder.tsx
@@ -1,8 +1,18 @@
+import { cn } from "@/lib/cn";
 import { Image } from "lucide-react";
 
-export default function ImagePlaceholder() {
+export type ImagePlaceholderProps = {
+    className?: string;
+};
+
+export default function ImagePlaceholder({ className }: ImagePlaceholderProps) {
     return (
-        <div className="flex bg-slate-200 dark:bg-gray-600 w-1/3 h-1/3 items-center justify-center text-slate-600 dark:text-black">
+        <div
+            className={cn(
+                "flex bg-slate-200 dark:bg-gray-600 w-1/3 h-1/3 items-center justify-center text-slate-600 dark:text-black",
+                className,
+            )}
+        >
             <Image size={64} />
         </div>
     );
