b9b369bdcc655e115273c6ed3963c79b551d43b8
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-10-27 16:42:04 -0700

    change: improve plopfile generator for stories

--- generators/story/index.js
+++ generators/story/index.js
@@ -1,3 +1,9 @@
+import path from "node:path";
+import fs from "node:fs";
+
+const featuresDir = path.join(process.cwd(), "app/features");
+const features = fs.readdirSync(featuresDir);
+
 /**
  *
  * @type {import('plop').PlopGenerator}
@@ -10,14 +16,38 @@ const storyGenerator = {
             name: "name",
             message: "component name",
         },
+        {
+            type: "list",
+            name: "feature",
+            message: "Which feature does this component belong to?",
+            choices: ["components", ...features],
+            default: 0,
+            when: () => features.length > 0,
+        },
+        {
+            type: "input",
+            name: "folder",
+            message: "folder in components",
+            when: ({ feature }) => !feature || feature === "components",
+        },
     ],
-    actions: () => {
-        const generatePath = "testing/stories";
+    actions: (answers) => {
+        const srcFolder =
+            !answers.feature || answers.feature === "components"
+                ? `components/${answers.folder}`
+                : `features/${answers.feature}`;
+        const generatePath =
+            !answers.feature || answers.feature === "components"
+                ? "testing/stories/components/{{folder}}"
+                : "testing/stories/features/{{feature}}";
         return [
             {
                 type: "add",
                 path: `${generatePath}/{{properCase name}}.stories.tsx`,
                 templateFile: "generators/story/component.stories.tsx.hbs",
+                data: {
+                    srcFolder,
+                },
             },
         ];
     },
