df7dbfa4f9fef7c2aae7af60777b7be236dcfa6e
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-10-29 20:27:27 -0700

    fix: vite plugin definitions

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "@tanstack/start/config";
 import viteTsConfigPaths from "vite-tsconfig-paths";
-
-export const viteConfig = {
-    plugins: [viteTsConfigPaths()],
-} as const;
 export default defineConfig({
     vite: {
-        // @ts-expect-error - some odd any typings
-        plugins: () => viteConfig.plugins,
+        plugins: [
+            // this is the plugin that enables path aliases
+            viteTsConfigPaths({
+                projects: ["./tsconfig.json"],
+            }),
+        ],
     },
 });
