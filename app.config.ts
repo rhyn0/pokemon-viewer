3d9db4b9e3b8c93fd11fa0e3661a4c7b92028a2a
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-11-18 23:34:58 -0800

    try some things to get build to work

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "@tanstack/start/config";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 export default defineConfig({
     vite: {
         plugins: [
             // this is the plugin that enables path aliases
             viteTsConfigPaths({
                 projects: ["./tsconfig.json"],
+            }),
+            TanStackRouterVite({
+                autoCodeSplitting: true,
             }),
         ],
     },
